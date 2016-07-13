bf9da0fd7c16704f39dc58e210287b22db1d1bd6
Sam Lindstrom <lindstrom.s@gmail.com>
2016-07-12 18:44:47 -0600

    More Twilio experimentation

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -7,24 +7,33 @@ class SmsController < ApplicationController
       body: 'Rails TEST'
     )
   end
-  
+
+     #Params: {"ToCountry"=>"US", "ToState"=>"NJ", "SmsMessageSid"=>"SMca238e8b0ad0f9e0fe1399af8160497e", "NumMedia"=>"0", "ToCity"=>"TEANECK", "FromZip"=>"68845", "SmsSid"=>"SMca238e8b0ad0f9e0fe1399af8160497e", "FromState"=>"NE", "SmsStatus"=>"received", "FromCity"=>"KEARNEY", "Body"=>"Hi", "FromCountry"=>"US", "To"=>"+12015784557", "ToZip"=>"07666", "NumSegments"=>"1", "MessageSid"=>"SMca238e8b0ad0f9e0fe1399af8160497e", "AccountSid"=>"ACfb7fc67e55e599355eaa728c323068fc", "From"=>"+13082934761", "ApiVersion"=>"2010-04-01", "controller"=>"sms", "action"=>"reply"} 
+
   def reply
-    # boot_twilio
+    @params = params
 
-    @account = @client.account
-
-
-    
-
-    byebug
-
-    response = Twilio::TwiML::Response.new do |r|
-      r.Message 'Hey guys'
+    if params['Body'] == "Start" 
+      render 'reply.xml.erb', :content_type => 'text/xml'
+      start_transaction_logging
     end
+  end
 
-    response.to_xml
+  def start_transaction_logging
+    @params = params
+    @location = @params['Body']
+    render 'start.xml.erb', :content_type => 'text/xml'
+  end
 
-    render 'process_sms.xml.erb', :content_type => 'text/xml'
+  def send_message_via_sms(message)
+    boot_twilio
+
+    @app_number = ENV['twilio_number']
+    @client.account.messages.create(
+      from: @app_number,
+      to: self.phone_number,
+      body: message,
+    )
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'sms#home'
+  
+  get '/sms/reply' => 'sms#reply'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
